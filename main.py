5b2f347ec97c06840558c33cef3d64b44108b454
RS6 <88463690+Syndrosia@users.noreply.github.com>
2022-07-10 17:34:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 cfg = [
    "point", # set to "dot" for an output of "three dot one-four" when you input "3.14" and set to "." for an output of "seven . eight-three" when you input "7.83"
-   "thousand", # set to "teendrud" for things such as "sixteen-hundred and twenty-three" when you input "1623"
+   "thousand", # set to "teendred" for things such as "sixteen-hundred and twenty-three" when you input "1623"
 ]
 
 deci = False 
